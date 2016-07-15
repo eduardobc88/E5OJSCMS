985783975718715ef8ae804a4fa955fe83d4284b
eduardobc <eduardobc.88@gmail.com>
2016-07-15 17:16:59 -0500

    E5OJS : Search 90%

--- routes/back-end.js
+++ routes/back-end.js
@@ -2112,6 +2112,75 @@ router.delete('/e5ojs-media-api/', function(req, res, next){
 /* end e5ojs admin pages routers */
 
 
+
+
+
+
+
+
+
+
+
+
+/* start e5ojs admin search routers */
+router.get('/search/', function(req, res, next) {
+    // get page with validate session
+    var search_word = req.param('search');
+    console.log(" ==== SEARCH ==== ",search_word);
+    e5ojs_validate_admin_session_callback(req, res, function(user_data) {
+        e5ojs_search(search_word, function(result_search){
+            res.render('back-end/e5ojs-search-result', { page_data: e5ojs_global_data.admin_pages['settings'], e5ojs_global_data:e5ojs_global_data, e5ojs_user_data:user_data.e5ojs_user_data[0], result_search:result_search });
+        });
+    });
+});
+
+
+
+function e5ojs_search(key_words, callback) {
+    // before this you need indexes the each documents
+    // db.e5ojs_page.createIndex({"page_title":"text"})
+    var result_search = [];
+    db.e5ojs_page.find({$text: {$search:key_words} }, function(err, page_result){
+        if( page_result.length > 0 ) {
+            for( page_key in page_result ) {
+                result_search.push( {
+                    id: page_result[page_key].page_id,
+                    title: page_result[page_key].page_title,
+                    url: host_url+"/admin/page/action/edit/"+page_result[page_key].page_id,
+                });
+            }
+        }
+        // get post types
+        db.e5ojs_post_type.find({},function(err, post_types_result){
+            var post_types = [];
+            for( post_type_key in post_types_result ) {
+                post_types.push({post_type_id:post_types_result[post_type_key].post_type_id, slug:post_types_result[post_type_key].post_type_slug});
+            }
+            db.e5ojs_post.find({$text: {$search:key_words} }, function(err, post_result){
+                if( post_result.length > 0 ) {
+                    for( post_key in post_result ) {
+                        for( pp_key in post_types ) {
+                            if( post_types[pp_key].post_type_id == post_result[post_key].post_post_type_id ) {
+                                result_search.push( {
+                                    id: post_result[post_key].post_id,
+                                    title: post_result[post_key].post_title,
+                                    url: host_url+"/admin/post-type/"+post_types[pp_key].slug+"/action/edit/"+post_result[post_key].post_id,
+                                });
+                                break;
+                            }
+                        }
+                    }
+                }
+                callback(result_search);
+            });
+        });
+
+
+    });
+}
+/* end e5ojs admin search routers */
+
+
 /* ============== end e5ojs router ============== */
 
 
